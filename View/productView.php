58193790c7f685863c1366666cf02511a783b94b
sunzid02 <sunzid02@gmail.com>
2020-06-02 10:52:14 +0600

    form body ready starts

--- View/productView.php
+++ View/productView.php
@@ -1,61 +1,74 @@
 <?php include('header.php'); ?>
 
-    <div class="page-wrapper bg-red p-t-180 p-b-100 font-robo">
-        <div class="wrapper wrapper--w960">
-            <div class="card card-2">
-                <div class="card-heading"></div>
-                <div class="card-body">
-                    <h2 class="title">Registration Info</h2>
-                    <form method="POST">
-                        <div class="input-group">
-                            <input class="input--style-2" type="text" placeholder="Name" name="name">
-                        </div>
-                        <div class="row row-space">
-                            <div class="col-2">
-                                <div class="input-group">
-                                    <input class="input--style-2 js-datepicker" type="text" placeholder="Birthdate" name="birthday">
-                                    <i class="zmdi zmdi-calendar-note input-icon js-btn-calendar"></i>
-                                </div>
-                            </div>
-                            <div class="col-2">
-                                <div class="input-group">
-                                    <div class="rs-select2 js-select-simple select--no-search">
-                                        <select name="gender">
-                                            <option disabled="disabled" selected="selected">Gender</option>
-                                            <option>Male</option>
-                                            <option>Female</option>
-                                            <option>Other</option>
-                                        </select>
-                                        <div class="select-dropdown"></div>
-                                    </div>
-                                </div>
+<div class="page-wrapper bg-red p-t-180 p-b-100 font-robo">
+    <div class="wrapper wrapper--w960">
+        <div class="card card-2">
+            <div class="card-heading"></div>
+            <div class="card-body">
+                <h2 class="title">Product </h2>
+                <form method="">
+
+                    <!-- amount -->
+                    <div class="input-group">
+                        <input class="input--style-2" type="number" min="0" placeholder="Amount" name="amount" maxlength="20" id="amount" required>
+                    </div>
+
+                    <!-- buyer, receipt_id -->
+                    <div class="row row-space">
+                        <!-- buyer -->
+                        <div class="col-2">
+                            <div class="input-group">
+                                <input class="input--style-2" type="text" placeholder="Buyer" name="buyer" maxlength="255" id="buyer" required>
                             </div>
                         </div>
-                        <div class="input-group">
-                            <div class="rs-select2 js-select-simple select--no-search">
-                                <select name="class">
-                                    <option disabled="disabled" selected="selected">Class</option>
-                                    <option>Class 1</option>
-                                    <option>Class 2</option>
-                                    <option>Class 3</option>
-                                </select>
-                                <div class="select-dropdown"></div>
+
+
+                        <!-- receipt_id  -->
+                        <div class="col-2">
+                            <div class="input-group">
+                                <input class="input--style-2" type="text" placeholder="Receipt id" name="receiptId" maxlength="20" id="receiptId" required>
                             </div>
                         </div>
-                        <div class="row row-space">
-                            <div class="col-2">
-                                <div class="input-group">
-                                    <input class="input--style-2" type="text" placeholder="Registration Code" name="res_code">
-                                </div>
+                    </div>
+
+
+
+                    <!-- items -->
+                    <div class="input-group">
+                        <input class="input--style-2" type="number" min="0" placeholder="Items" name="items" maxlength="255" id="items" required>
+                    </div>
+
+                    <!-- buyer_email  -->
+                    <div class="input-group">
+                        <input class="input--style-2" type="email" min="0" placeholder="Buyer email" name="buyerEmail" maxlength="50" id="buyerEmail" required>
+                    </div>
+
+
+                    <div class="input-group">
+                        <div class="rs-select2 js-select-simple select--no-search">
+                            <select name="class">
+                                <option disabled="disabled" selected="selected">Class</option>
+                                <option>Class 1</option>
+                                <option>Class 2</option>
+                                <option>Class 3</option>
+                            </select>
+                            <div class="select-dropdown"></div>
+                        </div>
+                    </div>
+                    <div class="row row-space">
+                        <div class="col-2">
+                            <div class="input-group">
+                                <input class="input--style-2" type="text" placeholder="Registration Code" name="res_code">
                             </div>
                         </div>
-                        <div class="p-t-30">
-                            <button class="btn btn--radius btn--green" type="submit">Search</button>
-                        </div>
-                    </form>
-                </div>
+                    </div>
+                    <div class="p-t-30">
+                        <button class="btn btn--radius btn--green" type="submit">Search</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
+</div>
 
 <?php include('footer.php'); ?>
